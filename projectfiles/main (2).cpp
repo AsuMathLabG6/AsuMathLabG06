6fd6f635d240e67bee523d278e40742c543512d2
Muhammed Sabry <muhammed.sabry.refai@gmail.com>
2018-01-27 17:57:24 +0200

    Fixed power

--- projectfiles/main (2).cpp
+++ projectfiles/main (2).cpp
@@ -590,7 +590,7 @@ CMatrix &power_matrix(CMatrix &matrix, int number)
 		return matrix;
 	else
 	{
-		for (int i = 0; i < number; i++)
+		for (int i = 0; i < number-1; i++)
 			temp = temp*matrix;
 	}
 	return temp;
