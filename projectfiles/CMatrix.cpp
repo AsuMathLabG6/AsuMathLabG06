1818af7b557fae5ecb95d5a789d84b15087826b3
Mohamed <30885495+1OneOfAKind1@users.noreply.github.com>
2018-01-28 23:11:29 +0200

    Update CMatrix.cpp

--- projectfiles/CMatrix.cpp
+++ projectfiles/CMatrix.cpp
@@ -2,6 +2,7 @@
 #include "stdarg.h"
 #include <algorithm>
 #include <math.h>
+#include "stdio.h"
 CMatrix::CMatrix()
 {
 	nR = nC = 0;
@@ -119,6 +120,10 @@ void CMatrix::copy(string s)
 		line = strtok_r(NULL, lineSeparators, &lineContext);
 	}
 	delete[] buffer;
+	if (nC != nR)
+	{
+        throw("Missing Element");
+	}
 }
 void CMatrix::reset()
 {
@@ -423,7 +428,7 @@ CMatrix& CMatrix::getTranspose(){
 }
 CMatrix& CMatrix::getInverse(){
 if(FastestDeterminant()==0)
-    throw (new string ("error zero determinent invalid division"));
+    throw ("error zero determinent invalid division");
  CMatrix a(nR,nC);
  CMatrix A;
  double d ;
