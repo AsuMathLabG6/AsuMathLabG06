01b17ab9a9be30f232d47332fd0b0a70ac878876
Mohamed_Hussien <32989600+teamleader6@users.noreply.github.com>
2018-01-29 19:39:16 +0200

    getting organized

--- projectfiles/main.cpp
+++ projectfiles/main.cpp
@@ -8,7 +8,6 @@
 #include <fstream>
 using namespace std;
 #define size 100
-//enum MI{ MI_ZEROS, MI_ONES, MI_EYE, MI_RAND, MI_VALUE };
 CMatrix& eye(int nr,int nc)
 {
 static CMatrix w;
@@ -78,11 +77,10 @@ int main(int argc, char* argv[])
 try{
 char array_chars [size];
 CMatrix* array_matrices = new CMatrix [size] ;
-
 //if (argc == 2)
 //{
 //argv[1]
-ifstream infile("/home/mido/Downloads/trickyexample.m");
+ifstream infile("/home/mido/Downloads/advexample(copy).m");
 if (!infile.bad())
 {
    string get_input , body_matrix , body_math , output , concatenate , decision , result1 ;
@@ -371,17 +369,6 @@ infile.close();
 catch(char const* error){ cout<<"Error: "<<error<<endl; }
 return 0;
 }
-/*bool is_end(char* text)
-{
-    bool found_exp = false ;
-    for(int i=0;i<strlen(text);i++)
-    {
-        char c = text[i] ;
-       if(c =='='||((c >='A'&&c <='Z')||(c >='a'&&c <='z'))|| c=='['||c==']'||c==','||c==';'||c>='0'||c<='9')
-            found_exp = true;
-    }
-    return found_exp;
-}*/
 char trim(string text)
 {
     for(int i=0;i<text.length();i++)
@@ -1231,11 +1218,11 @@ string check_matrix_in (string matrix,CMatrix* array_matrices,char* array_chars)
     {
         if(text[i]>='A'&&text[i]<='Z')
         {
-           if(get_index(array_chars,text[i])==1)
+           if(array_matrices[get_index(array_chars,text[i])].getnC()==1&&array_matrices[get_index(array_chars,text[i])].getnR()==1)
             {
                 temp = to_string(array_matrices[get_index(array_chars,text[i])].get_values(0,0)) ;
                 matrix.replace(i+grow_by,1,temp);
-                grow_by = temp.length();
+                grow_by = temp.length()-1;
                 temp.clear();
             }else
             {
@@ -1244,8 +1231,8 @@ string check_matrix_in (string matrix,CMatrix* array_matrices,char* array_chars)
                 else if(matrix.find("]",i+1) > matrix.find("[",i+1))
                     add_comma = 1 ;
                 temp = getstring(array_matrices[get_index(array_chars,text[i])],add_comma) ;
-                matrix.replace(i+(grow_by-1),1,temp);
-                grow_by = temp.length();
+                matrix.replace(i+grow_by,1,temp);
+                grow_by = temp.length()-1;
                 temp.clear();
             }
         }
@@ -1402,198 +1389,3 @@ string check_operation_in (string matrix)
     delete[]text;
     return matrix ;
 }
-/*
-#include <iostream>
-#include "CMatrix.h"
-#include "stdarg.h"
-#include <algorithm>
-using namespace std;
-CMatrix& eye(int nr,int nc)
-{
-static CMatrix w;
-CMatrix temp(nr,nc,2);
-w=temp;
-return w;
-}
-CMatrix& rand(int nr,int nc)
-{
-static CMatrix w;
-CMatrix temp(nr,nc,3);
-w=temp;
-return w;
-}
-CMatrix& zeros(int nr,int nc)
-{
-static CMatrix w;
-CMatrix temp(nr,nc,0);
-w=temp;
-return w;
-}
-CMatrix& ones(int nr,int nc)
-{
-static CMatrix w;
-CMatrix temp(nr,nc,1);
-w=temp;
-return w;
-}
-int main()
-{
-    CMatrix A ;
-    //A = "[1.2 3.4 5.6 7.8 1.0 3.2 2.1 2.3 1.4 2.6 2.7 2.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 3.7 1.2 3.4 5.6 7.8 9.0 1.2 2.1 2.3 2.4 1.6; 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4; 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 7.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 4.1 2.3; 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 1.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1; 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2; 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 17.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0; 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8; 9.0 2.2 2.1 2.3 2.4 1.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 2.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6; 7.4 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4; 5.6 7.8 9.0 2.2 4.4 2.3 2.4 2.6 2.7 1.2 3.4 5.6 2.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2; 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 1.8 9.0 1.2 2.1 2.3 2.4 8.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7; 1.2 3.4 5.6 7.8 9.0 2.2 5.1 4.3 12.4 2.6 2.7 1.2 3.4 5.6 97.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 3.4 2.6; 2.7 1.2 3.4 5.6 7.5 9.0 2.2 2.1 3.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 5.1 2.3 2.4 5.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4; 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 8.2 2.1 2.3; 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 6.6 2.7 1.2 3.4 5.6 2.8 9.0 1.2 2.1; 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 5.1 2.3 2.4 0.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 0.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2; 0.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0; 4.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 5.2 2.1 2.3 2.4 9.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8; 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 5.3 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.3; 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 3.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 3.4 2.6 3.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 8.4 2.6 2.7 1.2 3.4; 5.6 7.8 9.0 2.2 2.1 2.3 3.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 1.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2; 3.4 1.6 7.8 9.0 2.2 3.1 6.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7; 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 4.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 1.6; 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 1.4 2.6 2.7 1.2 3.4 9.6 7.8 9.0 2.2 2.1 2.3 2.4; 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 1.4 2.6 2.7 2.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3; 2.4 2.6 2.7 1.2 3.4 5.6 7.8 1.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 8.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 2.6 7.8 9.0 2.2 2.1; 7.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 3.6 1.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2; 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 7.2 3.4 5.6 7.8 9.0; 2.2 2.1 2.3 2.4 2.6 7.7 9.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.9 2.4 2.6 2.7 1.2 3.4 5.6 7.8; 9.0 2.2 2.1 2.3 2.4 2.6 7.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6; 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4; 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 2.7 1.2 3.4 5.6 7.8 9.0 2.2 2.1 2.3 2.4 2.6 5.7 9.3 3.4 5.6 3.8 9.0 2.2 5.1 2.3 2.4 2.6 2.7 1.2];";
-   // cout<<A.FastestDeterminant()<<endl;
-    //CMatrix B ;
-   // A="[1 -1 0 2; 2 1 0 0; 1 1 2 2; 0 0 1 1];";
-    //cout<<"\n"<<A;
-    //cout<<"\n"<<B;
-    //CMatrix C=A+B;
-    //cout<<"\n"<<C;
-    //C=A-B;
-    //cout<<"\n"<<C;
-    //C=A*B;
-    //cout<<"\n"<<C;
-    //C=A/B;
-    //cout<<A.FastestDeterminant()<<endl;
-    cout<<eye(4,4)<<"\n";
-    cout<<rand(4,4)<<"\n";
-    cout<<ones(4,4)<<"\n";
-    cout<<zeros(4,4)<<"\n";
-    A=ones(3,6);
-    cout<<A.sin_matrix()<<"\n";
-    return 0;
-}
-istream& operator >> (istream &is, CMatrix& m)
-{
-	string s;
-	getline(is, s, ']');
-	s += "]";
-    CMatrix v (s);
-	m = v;
-	return is;
-}
-ostream& operator << (ostream &os, CMatrix& m)
-{
-	os << m.getString();
-	return os;
-}
-/*string handling_operations(string s)
-{
-    string temp;
-    while(s.length()==0)
-    {
-        if(s.find("(")!=0)
-        {
-        temp=s.substr(s.find("(")+1,s.find(")")-(s.find("(")+1));
-
-        }
-    }
-}
-string do_operation(string x)
-{
-    int length=x.length();
-    string arr_op [5] = {"^","/","*","+","-"};
-    for(int j=0;j<5;j++)
-    {
-        for(int i=0;i<length;i++)
-            {
-
-            }
-    }
-}
-*/
-
-/*	string get_input;
-	string body_matrix;
-	bool start_package,end_package,is_semicolon=false,start_operation=false;
-	int end;
- 	char c;
-	while(getline(infile,get_input))
-		{
-	is_semicolon=false;
-	int length=get_input.length();
-	if(get_input=="\r"||get_input=="")
-	{
-        continue;
-    }
-	end=get_input.find("]");
-	if(get_input.find("[")!=-1)
-		start_package=true;
-    else
-        start_package=false;
-	if(get_input.find("]")!=-1)
-	{
-		end_package=true;
-	}else{
-		end_package=false;
-		}
-	if(start_package)
-	{
-        c=*((const char*)get_input.substr(0,1).c_str());
-		insert_name(array_chars,c);
-	}
-	if(end_package&&start_package)
-	{
-		body_matrix=get_input.substr(4,end-3);
-		reserve(array_matrices, body_matrix);
-		body_matrix.clear();
-		if(get_input.substr(end+1,1)==";")
-			is_semicolon=true;
-		if(!is_semicolon)
-		{
-			cout<<array_matrices[get_index(array_chars,c)].getString()<<endl;
-		}
-	}
-	if(end_package&&!start_package)
-	{
-		body_matrix+=get_input.substr(0,end+1);
-		reserve(array_matrices, body_matrix);
-		body_matrix.clear();
-		if(get_input.substr(end+1,1)==";")
-			is_semicolon=true;
-		if(!is_semicolon)
-		{
-			cout<<array_matrices[get_index(array_chars,c)].getString();
-		}
-	}
-	if(!end_package&&!start_package&&get_input.find("=")!=-1)
-		{
-		start_operation = true;
-		string req;
-		char* g = (char*)get_input.c_str();
-		char* spearators = " ";
-		char* temp = strtok(g,spearators);
-		while(temp)
-		{
-            req += string(temp);
-            temp = strtok(NULL,spearators);
-		}
-		get_input = req;
-		c=*((const char*)get_input.substr(0,1).c_str());
-		insert_name(array_chars, c);
-		if(get_input.find("'")!=-1)
-		{
-		if(get_input.find(";")==4)
-			is_semicolon=true;
-		check_print(operationg(array_matrices,array_chars,num_elements,*((const char*)get_input.substr(2,1).c_str()),get_input.substr(3,1),c,0,0),is_semicolon);
-		}else if(get_input.find(".")==-1)
-		{
-		if(get_input.find(";")!=-1)
-			is_semicolon=true;
-		check_print(operationg(array_matrices,array_chars,num_elements,*((const char*)get_input.substr(2,1).c_str()),get_input.substr(3,1),c,*((const char*)get_input.substr(4,1).c_str())),is_semicolon);
-		}else if((*((const char*)get_input.substr(2,1).c_str())>='A'||*((const char*)get_input.substr(2,1).c_str())<='Z')&&(*((const char*)get_input.substr(5,1).c_str())>='A'||*((const char*)get_input.substr(5,1).c_str()))&&get_input.substr(3,1)==".")
-		{
-		if(get_input.find(";")!=-1)
-			is_semicolon=true;
-		check_print(operationg(array_matrices,array_chars,num_elements,*((const char*)get_input.substr(5,1).c_str()),get_input.substr(3,2),c,0,atof(get_input.substr(2,1).c_str())),is_semicolon);
-		}
-		}
-		if (((!end_package&&start_package)||(!end_package&&!start_package))&&(!start_operation))
-	{
-	//&&get_input.substr(length-1,1)=="\n"
-		if(get_input.substr(length-1,1)=="\r")
-		{
-            if(start_package)
-			body_matrix+=get_input.substr(4,length-5);
-			else
-			body_matrix+=get_input.substr(0,length-1);
-		}
-	}*/
